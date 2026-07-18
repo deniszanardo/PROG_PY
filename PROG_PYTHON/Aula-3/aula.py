368a6e216d57f7ab58c841153c2812473b8fed35
Denis Zanardo <deniszanardo@gmail.com>
2026-07-17 21:45:47 -0300

    Add files via upload

---
+++ PROG_PYTHON/Aula-3/aula.py
@@ -0,0 +1,60 @@
+
+
+# # imprima algo no ambiente de teste
+
+
+# # # dados primitivos
+
+
+# # print(type(10))
+# # print(type("strings ==  textos"))
+# # print(type(10 + 2)) # int inteiro
+# # print(type(5.5 - 1.2)) # float real
+# # print(type(False)) # boleanos
+
+
+
+
+
+# nota1 = 3
+# nota2 = 5
+# nota3 = 4
+
+# media = (nota1 + nota2 + nota3)/3
+# print(media)
+
+# aprovado = media >=7 
+# recuperacao = media >=5 and media > 7
+# reprovado = media <=5
+
+# print(f'')
+
+# # OBJETIVO - FAZER A MÉDIA DAS 3 NOTAS DE UM ALUNO, PARA PASSAR,O ALUNO PRECISA ALCANÇAR >=7 (PASSAR), >=5 E  <7 RECUPERAÇÃO, <5 (REPROVADO).   
+
+
+# nota_1 = 10
+# nota_2 = 6
+# nota_3 = 9
+
+
+# media = (nota_1 + nota_2 + nota_3)/3
+# print('NOTA:')
+# print(round(media, 2))
+
+
+# aprovado = media >= 7
+# recuperacao =  media >= 5 and media < 7
+# reprovado = media < 5
+
+
+# resultado =  (media >=7 and print('Aprovado')) or  (media >=5 and media < 7 and print('Recuperação')) or (media <5 and print('reprovado'))  
+
+
+# # print('situação do aluno: ')
+# # print('Aprovado:')
+# # print(aprovado)
+# # print('Recuperação:')
+# # print(recuperacao)
+# # print('Reprovado:')
+# # print(reprovado)
+
