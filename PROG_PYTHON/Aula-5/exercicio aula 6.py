34e34a8eafcd120c268b5c9457252b3dd4e8140a
Denis Zanardo <deniszanardo@gmail.com>
2026-07-23 19:23:37 -0300

    Add files via upload

---
+++ PROG_PYTHON/Aula-5/exercicio aula 6.py
@@ -0,0 +1,40 @@
+# Calcule o módulo ( % ) de 17 por 4.
+
+
+# Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+# n = 9 * 3
+# n1 = n**2
+# print(n1)
+
+# # Calcule a raiz quadrada de 81.
+
+# # Adicione 20 a 3 vezes 4.
+# n = 20 + (3*4)
+# print(n)
+
+# # Multiplique 15 por 2 e, em seguida, subtraia 7.
+# n = (15 * 2) - 7
+# print(n)
+
+# # Eleve 5 ao cubo.
+# n = 5 ** 3
+# print(n)
+
+# # Calcule a média de 17, 21 e 25.
+
+# n = (17 + 21 + 25) / 3
+# print(n)
+
+# # Multiplique 11 por 2 e adicione 7.
+# n = (11 * 2) + 7
+# print(n)
+# # Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+# n = 15 - (3 * 8)
+# print(n)
+# n1 = n / 2
+# print(n1)
+
+# # Eleve 2 à potência de 10.
+
+# n = 2 ** 10
+# print(n)
